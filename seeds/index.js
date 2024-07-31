5aa32e3e426f5243c61f7974e47bc7cbeaccfc82
juliette maloke <your@email.com>
2024-07-31 16:25:11 -0500

    Add seeds directory with initial database seed data

---
+++ seeds/index.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const seedCategories = require('./category-seeds');
+const seedProducts = require('./product-seeds');
+const seedTags = require('./tag-seeds');
+const seedProductTags = require('./product-tag-seeds');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await seedCategories();
+  console.log('\n----- CATEGORIES SEEDED -----\n');
+
+  await seedProducts();
+  console.log('\n----- PRODUCTS SEEDED -----\n');
+
+  await seedTags();
+  console.log('\n----- TAGS SEEDED -----\n');
+
+  await seedProductTags();
+  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+
+  process.exit(0);
+};
+
+seedDatabase();
